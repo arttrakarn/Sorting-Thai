78904d9daa2eca19dda4caaf9f93deffa200dee1
trakarn <arttrakarn123@gmail.com>
2016-09-28 11:54:47 +0700

    ListView

--- app/src/main/java/com/example/nrru/panthaisong/trakarn/sortingthai/SortListView.java
+++ app/src/main/java/com/example/nrru/panthaisong/trakarn/sortingthai/SortListView.java
@@ -2,12 +2,31 @@ package com.example.nrru.panthaisong.trakarn.sortingthai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class SortListView extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private String[] strings;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sort_list_view);
-    }
-}
+
+        listView = (ListView) findViewById(R.id.sortListView);
+        strings = getResources().getStringArray(R.array.soft);
+
+        // Create ListView
+
+        SortAdapter sortAdapter = new SortAdapter(this, strings);
+        listView.setAdapter(sortAdapter);
+
+
+    } //Main Method
+} // Main Class
